ba42001151dcf27e1d2965b9e5e26dd20c4841e7
Pedro Cardoso <pedro.contato24@gmail.com>
2023-08-08 22:00:09 -0300

    uploadCallBartender-frontend

---
+++ ChamarGarçom/app.js
@@ -0,0 +1,8 @@
+const callBtn = document.getElementById("btnCall");
+
+callBtn.addEventListener("click", chamarGarcom);
+
+function chamarGarcom(){
+    alert("Garçom a caminho, aguarde um momento!")
+    
+}
